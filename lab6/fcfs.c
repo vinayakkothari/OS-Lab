eba0e411e5ef8ec65b10be7f418bd8a5f6d7e578
vinayakkothari <123492606+vinayakkothari@users.noreply.github.com>
2024-08-30 10:35:17 +0530

    Add files via upload

---
+++ lab6/fcfs.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+
+void main() {
+    int n, i, a[10], b[10], no[10], temp, wt[10], tw = 0, ta[10], tt = 0;
+    float avgw, avgt;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        no[i] = i;
+    }
+
+    for (i = 0; i < n; i++) {
+        printf("Enter Arrival Time for process %d: ", i);
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 0; i < n; i++) {
+        printf("Enter Burst Time for process %d: ", i);
+        scanf("%d", &b[i]);
+    }
+
+    for (i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (a[j] > a[j + 1]) {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+
+                temp = b[j];
+                b[j] = b[j + 1];
+                b[j + 1] = temp;
+
+                temp = no[j];
+                no[j] = no[j + 1];
+                no[j + 1] = temp;
+            }
+        }
+    }
+
+    wt[0] = 0;
+    for (i = 1; i < n; i++) {
+        wt[i] = 0;
+        for (int j = 0; j < i; j++) {
+            wt[i] += b[j];
+        }
+        wt[i] -= a[i];
+    }
+
+    for (i = 0; i < n; i++) {
+        ta[i] = b[i] + wt[i];
+        tw += wt[i];
+        tt += ta[i];
+    }
+
+    avgw = (float)tw / n;
+    avgt = (float)tt / n;
+
+    printf("\n\tGantt Chart:\n");
+    printf("\t+");
+    for (i = 0; i < n; i++) {
+        printf("-------+");
+    }
+    printf("\n\t|");
+    for (i = 0; i < n; i++) {
+        printf("   %d   |", no[i]);
+    }
+    printf("\n\t+");
+    for (i = 0; i < n; i++) {
+        printf("-------+");
+    }
+    printf("\n\t0");
+    int c = 0;
+    for (i = 0; i < n; i++) {
+        c += b[i];
+        printf("\t%d", c);
+    }
+    printf("\n\n");
+
+    printf("\tProcess\tAT\tBT\tWT\tTAT\n");
+    for (i = 0; i < n; i++) {
+        printf("\t%d\t%d\t%d\t%d\t%d\n", no[i], a[i], b[i], wt[i], ta[i]);
+    }
+    printf("\nAverage WT is %.2f\n", avgw);
+    printf("Average TAT is %.2f\n", avgt);
+}
